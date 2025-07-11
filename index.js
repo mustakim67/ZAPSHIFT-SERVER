babe81fe367a7c8ee96335dd3aee45cad8491d99
mustakim67 <mahfujapple95@gmail.com>
2025-07-11 11:39:19 +0600

    fixed issue

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 //firebase sdk
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_PATH);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
